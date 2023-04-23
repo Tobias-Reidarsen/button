405db31037d8e82516267507ac77af7c7a781c71
tobias <tobias.reidarsen.working@gmail.com>
2023-04-23 15:59:28 +0200

    jkhgvb

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <script setup>
+import { ref } from "vue";
 import CounterBtn from "./components/CounterBtn.vue";
+
+const bool = ref(false);
 </script>
+
 <template>
-	<CounterBtn></CounterBtn>
+	<main>
+		<div class="container">
+			<header>
+				<h1>Notes</h1>
+				<button>+</button>
+				<div class="cards-container">
+					<div class="card">
+						<p class="main-text"></p>
+						<p class="date"></p>
+					</div>
+				</div>
+			</header>
+		</div>
+	</main>
 </template>
