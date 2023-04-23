33583cddb984ecccd805832255f75e487b96e4f6
tobias <tobias.reidarsen.working@gmail.com>
2023-04-23 15:07:52 +0200

    Now uses the button component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,6 @@
 <script setup>
-import { ref } from "vue";
-
-const count = ref(0);
-
-const inc = () => count.value++;
-const dec = () => count.value--;
+import CounterBtn from "./components/CounterBtn.vue";
 </script>
-
-<!-- <script> //Options version
-export default {
-	data() {
-		return {
-			count: 0,
-		};
-	},
-	methods: {
-		inc() {
-			console.log("inc() called");
-			this.count++;
-		},
-		dec() {
-			console.log("dec() called");
-			this.count--;
-		},
-	},
-};
-</script> -->
-
 <template>
-	<main>
-		<div>
-			<h4>The current count is</h4>
-			<h1>{{ count }}</h1>
-			<button @click="dec">-</button>
-			<button @click="inc">+</button>
-		</div>
-	</main>
+	<CounterBtn></CounterBtn>
 </template>
