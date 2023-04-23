aa9cdb956fd0b30a9b1ea6b52ded19def0df5235
tobias <tobias.reidarsen.working@gmail.com>
2023-04-23 16:11:04 +0200

    sdfgrdthj

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup>
-import { ref } from "vue";
-import CounterBtn from "./components/CounterBtn.vue";
-
-const bool = ref(false);
-</script>
-
 <template>
 	<main>
 		<div class="container">
@@ -13,11 +6,58 @@ const bool = ref(false);
 				<button>+</button>
 				<div class="cards-container">
 					<div class="card">
-						<p class="main-text"></p>
-						<p class="date"></p>
+						<p class="main-text">s</p>
+						<p class="date">q</p>
 					</div>
 				</div>
 			</header>
 		</div>
 	</main>
 </template>
+
+<style scoped>
+main {
+	height: 100vh;
+	width: 100vw;
+}
+
+.container {
+	max-width: 1000px;
+	padding: 10px;
+	margin: 0 auto;
+}
+
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+h1 {
+	font-weight: bold;
+	margin-bottom: 25px;
+	font-size: 75px;
+}
+button {
+	border: none;
+	padding: 10px;
+	width: 50px;
+	height: 50px;
+	cursor: pointer;
+	background-color: rgb(21, 20, 20);
+	border-radius: 100%;
+	color: white;
+	font-size: 20px;
+}
+.card {
+	width: 225px;
+	height: 225px;
+	background-color: red;
+	padding: 10px;
+	border-radius: 15px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	margin-right: 20px;
+	margin-left: 20px;
+}
+</style>
